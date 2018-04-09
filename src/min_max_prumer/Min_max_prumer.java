b1f4b90c4b8f60ee8e72ed483138137bc6086d15
jakub.stepanek <jakub.stepanek@W10A210.sps-cl.local>
2018-04-09 15:29:18 +0200

    prvni commit

---
+++ src/min_max_prumer/Min_max_prumer.java
@@ -0,0 +1,28 @@
+package min_max_prumer;
+import java.util.Scanner;
+public class Min_max_prumer {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in, "CP1250");
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        int soucet = 0; int pocet = 0;
+        // nekonečná smyčka
+        while (true) { 
+            pocet ++;
+            
+            System.out.print("vlož: "+pocet+". číslo: ");
+            int vstup = sc.nextInt ();
+            // když uživatel vloží nulu, program skončí
+            if (vstup == 0) break;
+            soucet = soucet + vstup;
+            // pokud vložil menší nebo větší číslo, zapamatuje si je
+            if (vstup < min) min = vstup;
+            if (vstup > max) max = vstup;
+            System.out.println("minimum: "+min);
+            System.out.println("maximum: "+max);
+            System.out.println("průměr: "+((float) soucet/pocet));
+            
+                        
+        }
+    }
+}
